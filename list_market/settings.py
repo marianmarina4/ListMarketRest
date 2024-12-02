d401a28a10ca7111fff7f6a74410b1da8f98c5bb
marianmarina4 <marianomarina04@hotmail.com>
2024-12-02 20:45:34 -0300

    Keep safe Secret Key

--- list_market/settings.py
+++ list_market/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2di9)ni19v%xzurnafv48k%czgzhq7g1lfy2g!)o4a7m3@9rm3'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
